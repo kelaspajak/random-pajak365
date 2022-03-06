43d8fdaf7b678554a8d3dc268a554942cda80c64
pajakbalikpapan <pajak.resources@gmail.com>
2022-03-06 17:42:59 +0800

    Update _document.tsx
    add inter font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,7 @@ class MyDocument extends Document {
                         href='https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
                         rel='stylesheet'
                     />
+                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
 
                     <script data-token={`${process.env.SPLITBEE_ANALYTICS_CODE}`} async src='https://cdn.splitbee.io/sb.js'></script>
 
@@ -19,14 +20,14 @@ class MyDocument extends Document {
                     <script
                         dangerouslySetInnerHTML={{
                             __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.GOOGLE_ANALYTICS_CODE}', {
-              page_path: window.location.pathname,
-            });
-          `,
-                        }}
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${process.env.GOOGLE_ANALYTICS_CODE}', {
+                        page_path: window.location.pathname,
+                        });
+                    `,
+                                    }}
                     />
                 </Head>
                 <body>
