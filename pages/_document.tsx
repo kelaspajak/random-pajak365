4886ecb19fab0ee822510dad584938809b7c4ec7
Tom Tom <70383056+pajakbalikpapan@users.noreply.github.com>
2022-09-12 09:06:48 +0800

    inter font update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,9 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-<link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap'
-            rel='stylesheet'
+        <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap"
+            rel="stylesheet"
           />
 
           <script
