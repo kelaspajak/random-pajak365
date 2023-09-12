b90585b44fdb0564962d88a8bccafbe8798b4cc2
lesssummer <57969740+kelaspajak@users.noreply.github.com>
2023-09-12 10:50:06 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,19 +3,3 @@
 /**
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
-
-// @ts-ignore
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/space',
-        destination: 'https://space.pajak365.com/',
-      },
-      {
-        source: '/resumepajak',
-        destination: 'https://resumepajak.pajak365.com/',
-      },
-    ];
-  },
-};
